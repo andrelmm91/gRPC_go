378b29f9503f9f566f7b71a86ffd116c11e50533
andrelmm91 <andre.lmm91@gmail.com>
2024-04-10 21:55:34 +0200

    grpc dial

---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "gRPCgo/greet/proto"
+
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceClient
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on: %v \n", err)
+	}
+
+	log.Printf("listening on %s \n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v\n", err)
+	}
+}
