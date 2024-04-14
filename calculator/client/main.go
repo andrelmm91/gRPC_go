d4dd5cbc63c65261a114af860f95a02869a715a8
andrelmm91 <andre.lmm91@gmail.com>
2024-04-14 14:57:04 +0200

    unary_grpc

---
+++ calculator/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "gRPCgo/calculator/proto"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to listen on: %v \n", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewCalculatorServicesClient(conn)
+
+	doSum(c)
+}
